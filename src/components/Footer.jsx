76617220a5e1f8890409f83ac20a3c31403966e2
unknown <ya.ramdane01@gmail.com>
2025-11-14 21:25:50 +0100

    add link in Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { assets } from '../assets/frontend_assets/assets'
+import { Link } from 'react-router-dom'
 
 const Footer = () => {
   return (
@@ -7,7 +8,7 @@ const Footer = () => {
         <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm'>
 
             <div>
-                <img src={assets.logo} className='mb-5 w-32' alt="" />
+                <Link to='/'><img src={assets.logo} className='w-36' alt="logo-forever" /></Link> 
                 <p className='w-full md:w-2/3 text-gray-600'>
                     texte a completer
                 </p>
@@ -17,10 +18,10 @@ const Footer = () => {
                 <p className='text-xl font-medium mb-5'>COMPAGNY</p>
 
                 <ul className='flex flex-col gap-1 text-gray-600'>
-                    <li>Accueil</li>
-                    <li>Boutique</li>
-                    <li>A propos de nous</li>
-                    <li>Nous contacter</li>
+                   <Link to='/'><li>Accueil</li></Link>
+                    <Link to='/collection'><li>Boutique</li></Link>
+                    <Link to='/about'><li>A propos de nous</li></Link>
+                    <Link to='/contact'><li>Nous contacter</li></Link>
                 </ul>
             </div>
 
