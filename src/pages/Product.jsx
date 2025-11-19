f75d00b6eb9286b90fe740234efd23e2c4045743
unknown <ya.ramdane01@gmail.com>
2025-11-19 20:39:49 +0100

    add la meilleure brochettes des dix

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,11 +1,42 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useContext } from 'react';
+import { useParams } from 'react-router-dom'
+import { ShopContext } from '../context/ShopContext';
 
 const Product = () => {
-  return (
-    <div>
-      
+
+  const {productId} = useParams();
+  const {products} = useContext(ShopContext);
+  const [productData,setProductData] = useState(false);
+  const [image,setImage] = useState('');
+
+  const fetchProductData = async ()=> {
+
+      products.map((item)=>{
+        if(item._id === productId){
+          setProductData(item)
+          setImage(item.image[0])
+          return null;
+        }
+      })
+
+  }
+
+  useEffect(()=>{
+    fetchProductData();
+  },[productId, products])
+  
+
+  return productData ? (
+    <div className='border-t-2 pt-10 transition-opacity ease-in duration-500 opacity-100'>
+      {/*Product Data */}
+      <div className='flex gap-12 sm:gap-12 flex-col sm:flex'>
+      {/*Product Images */}
+      </div>
+
+
     </div>
-  )
+  ) : <div className='opacity-0'></div>
 }
 
 export default Product
