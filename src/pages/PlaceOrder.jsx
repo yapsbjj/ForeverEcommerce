7a4129716742a993dd6dab37339a15fb2680ee03
unknown <ya.ramdane01@gmail.com>
2025-11-25 21:16:24 +0100

    start place order page

--- src/pages/PlaceOrder.jsx
+++ src/pages/PlaceOrder.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 const PlaceOrder = () => {
   return (
-    <div>
-      
+    <div className='flex flex-col sm:flex-row justify-between gap-4 pt-5 sm:pt-14 min-h[80vh] border-t '>
+        <div className='flex flex-col gap-4 w-full sm:max-w-[480px'>
+
+        </div>
     </div>
   )
 }
