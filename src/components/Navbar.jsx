567210c741c8fc443965ead9bef05b3effea6ae8
unknown <ya.ramdane01@gmail.com>
2025-11-28 20:44:08 +0100

    add login page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -47,7 +47,7 @@ const Navbar = () => {
             
 
                 <div className='group relative'>
-                    <img className='w-5 cursor-pointer' src={assets.profile_icon} alt="image de profil" />
+                   <Link to={'/login'}><img className='w-5 cursor-pointer' src={assets.profile_icon} alt="image de profil" /></Link>
                         <div className='group-hover:block hidden absolute dropdown-menu right-0 pt-4'>
                             <div className='flex flex-col gap-2 w-36 py-5 bg-slate-100 text-gray-500 rounded'>
                                 <p className='cursor-pointer hover:text-black'>Mon profil</p>
