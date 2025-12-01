ad54a49d8f3fc307de1e7a10a6b9fab16bae340a
unknown <ya.ramdane01@gmail.com>
2025-12-01 21:39:47 +0100

    add Contact page end finish the frontend

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import Title from '../components/Title'
+import { assets } from '../assets/frontend_assets/assets'
+import NewsletterBox from '../components/NewsletterBox'
 
 const Contact = () => {
   return (
     <div>
+      <div className='text-center text-2xl pt-10 border-t'>
+          <Title text1={'Nous'} text2={'Contacter'} />
+      </div>
+
+      <div className='my-10 flex flex-col justify-center md:flex-row gap-10 mb-28'>
+        <img className='w-full md:max-w-[480px]' src={assets.contact_img} alt="" />
+          <div className='flex flex-col justify-center items-start gap-6'>
+            <p className='font-semibold text-xl text-gray-600'>Notre Boutique</p>
+            <p className='text-gray-500'>15 place Vendôme <br />  75008 PARIS</p>
+            <p className='text-gray-500'>Tel: 01 34 48 96 32 <br /> Email: admin@forever.com</p>
+            <p className='font-semibold text-xl text-gray-600'>Recrutement à Forever</p>
+            <p className='text-gray-500'>Venez découvrir nos équipes <br /> et nos opportunités de carrières</p>
+            <button className='border border-black px-8 py-4 text-sm hover:bg-black hover:text-white transition-all duration-500'>Découvrir les offres d'emplois</button>
+          </div>
+      </div>
+
+      <NewsletterBox />
       
     </div>
   )
